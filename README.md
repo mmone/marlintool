06539ab76e40e537e77caba78c439117fc5be79e
Martin Mühlhäuser <bitbucket@mmone.de>
2017-04-23 13:54:16 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # marlintool
-A convenience shell script for setting up a standalone Marlin build environment on Raspberry Pi and Linux in general.
+This is a convenience shell script for setting up a standalone Marlin build environment on Raspberry Pi and Linux in general.
+
+If you are already running octoprint as a printserver on a Raspberry Pi it is very convenient to also build Marlin on it via ssh. This script sets up the necessary build environment and provides commands for building and uploading from the commandline. It uses the official Arduino toolchain for ARM. Everything is standalone, nothing is installed.
+
+It should also work on Linux in general. If you don’t build on ARM you will need to change the architecture though. Check the parameters at the beginning of the script for that.
+
+The script is setup by default to build the Marlin fork “Skynet3D” for the Anet A8 Prusa clone. If you want to build stock Marlin, change the “marlinRepositoryUrl” parameter respectively. You should also set the parameter “hardwareDefintionDirectory” to an empty string, this prevents the script from trying to copy the board definition that is needed for the A8.
+
+If you are running octopi on you Raspberry you need to disconnect it from your printer before uploading, otherwise the serial port is blocked.
+
+
+Commandline parameters
+
+-s –setup
+
+Download and configure the toolchain and the necessary libraries for building Marlin.
+
+-m –marlin
+
+Download Marlin sources.
+
+-v –verify
+
+Build without uploading.
+
+-u –upload
+
+Build and upload Marlin. If you are running octopi on you Raspberry you need to disconnect it before uploading otherwise the serial port is blocked.
+
+-b –backupConfig [file]
+
+Backup the Marlin configuration to the given file.
+
+-r –restoreConfig [file]
+
+Put the given configuration into the Marlin directory. Rename to Configuration.h implicitly.”
+
+-c  –clean
+
+Cleanup everything. Remove Marlin sources and Arduino toolchain\n\n”
+
+-p –port [port]
+
+Set the serialport for uploading the firmware. Overrides the default set in the script.
+
+-h –help
+
+Show help.
---
+++ marlintool.sh
@@ -0,0 +1,187 @@
+# Marlin fork optimized for the AnetA8 Prusa clone
+marlinRepositoryUrl="https://github.com/SkyNet3D/Marlin"
+
+# Original Marlin
+# marlinRepoUrl="https://github.com/MarlinFirmware/Marlin"
+
+# Anet board
+boardString="anet:avr:anetv1"
+
+# Arduino Mega
+# boardString="arduino:avr:mega:cpu=atmega2560"
+
+arduinoToolchainVersion="1.8.2"
+
+# Toolchain architecture
+arduinoToolchainArchitecture="linuxarm"
+# 32bit
+#arduinoToolchainArchitecture="linux32"
+# 64bit
+#arduinoToolchainArchitecture="linux64"
+
+# Serialport for uploading
+port="/dev/ttyUSB0"
+
+# Where to put the arduino toolchain
+arduinoDir="./arduino"
+
+# Where to checkout Marlin sources
+marlinDir="Marlin"
+
+# Build directory
+buildDir="./build"
+
+# The path to additional hardware defnitions for the arduino tool chain
+# eg. sanguino boards that live in "/arduino/hardware".
+# Set to an empty string if you dont need this.
+hardwareDefintionDirectory="./hardware/anet"
+
+
+
+function getArduinoToolchain
+{
+	echo -e "\nDownloading Arduino environment ...\n"
+	wget http://downloads-02.arduino.cc/arduino-"$arduinoToolchainVersion"-"$arduinoToolchainArchitecture".tar.xz
+	mkdir "$arduinoDir"
+	echo -e "\nUnpacking Arduino environment. This might take a while ... "
+	tar -xf arduino-"$arduinoToolchainVersion"-"$arduinoToolchainArchitecture".tar.xz -C "$arduinoDir" --strip 1
+	rm -R arduino-"$arduinoToolchainVersion"-"$arduinoToolchainArchitecture".tar.xz
+}
+
+
+## Get dependencies and move them in place
+function getDependencies
+{
+	echo -e "\nDownloading libraries ...\n"
+
+	git clone https://github.com/kiyoshigawa/LiquidCrystal_I2C.git
+	rm -rf "$arduinoDir"/libraries/LiquidCrystal_I2C
+	mv -f LiquidCrystal_I2C/LiquidCrystal_I2C "$arduinoDir"/libraries/LiquidCrystal_I2C
+	rm -rf LiquidCrystal_I2C
+
+	git clone https://github.com/lincomatic/LiquidTWI2.git
+	rm -rf "$arduinoDir"/libraries/LiquidTWI2
+	mv -f LiquidTWI2 "$arduinoDir"/libraries/LiquidTWI2
+	rm -rf LiquidTWI2
+
+	git clone https://github.com/olikraus/U8glib_Arduino.git
+	mv -f U8glib_Arduino "$arduinoDir"/libraries/U8glib_Arduino
+	rm -rf U8glib_Arduino
+}
+
+
+function getMarlin
+{
+	echo -e "\nCloning Marlin \"$marlinRepositoryUrl\"...\n"
+	git clone "$marlinRepositoryUrl" "$marlinDir" 
+	exit
+}
+
+
+function setupEnvironment
+{
+	echo -e "\nSetting up build environment in \"$arduinoDir\" ...\n"
+	getArduinoToolchain
+	getDependencies
+	installHardwareDefinition
+	exit
+}
+
+
+function installHardwareDefinition
+{
+	if [ "$hardwareDefintionDirectory" != "" ]; then
+	echo -e "\nInstalling board hardware definition ... \n"
+	
+	cp -R "$hardwareDefintionDirectory" "$arduinoDir"/hardware/
+	fi
+}
+
+
+function backupMarlinConfiguration
+{
+	echo -e "\nSaving marlin configuration to \"$1\"\n"
+	cp  Marlin/Marlin/Configuration.h "$1"
+	exit
+}
+
+
+function installMarlinConfiguration
+{
+	echo -e "\nMoving configuration in place at Marlin/Marlin/Configuration.h.\n"
+	cp "$1" Marlin/Marlin/Configuration.h
+	exit
+}
+
+
+function verifyBuild
+{
+	echo -e "\nVerifying build...\n"
+	./arduino/arduino --verify --verbose --board "$boardString" "$marlinDir"/Marlin/Marlin.ino --pref build.path="$buildDir"
+	exit
+}
+
+
+function buildAndUpload
+{
+	echo -e "\nBuilding and uploading Marlin build from \"$buildDir\" ...\n"
+	./arduino/arduino --upload --port "$port" --verbose --board "$boardString" "$marlinDir"/Marlin/Marlin.ino --pref build.path="$buildDir"
+	exit
+}
+
+function cleanEverything
+{
+	rm -Rf "$arduinoDir"
+	rm -Rf "$marlinDir"
+	rm -Rf "$buildDir"
+}
+
+function printDocu
+{
+	echo -e "\n\n-------------------------------------------------------------------------"
+   echo -e "    Comandline parameters:"
+   echo -e "-----------------------------------------------------------------------------\n\n"
+   echo -e "-s  --setup                 Download and configure the toolchain and the"
+   echo -e "                            necessary libraries for building Marlin.\n\n"
+   echo -e "-m  --marlin                Download Marlin sources.\n\n"
+   echo -e "-v  --verify                Build without uploading.\n\n"
+   echo -e "-u  --upload                Build and upload Marlin.\n\n"
+   echo -e "-b  --backupConfig  [file]  Backup the Marlin configuration to the given file.\n"
+   echo -e "-r  --restoreConfig [file]  Put the given configuration into the Marlin directory."
+   echo -e "                            Rename to Configuration.h implicitly.\n\n"
+   echo -e "-c  --clean                 Cleanup everything. Remove Marlin sources and Arduino toolchain\n\n"
+   echo -e "-p  --port [port]           Set the serialport for uploading the firmware."
+   echo -e "                            Overrides the default in the script.\n\n"
+   echo -e "-h  --help                  Show this doc.\n\n\n"
+	exit
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -p | --port )           shift
+                                port=$1
+                                ;;
+        -s | --setup )          setupEnvironment
+                                ;;
+        -m | --marlin )         getMarlin
+                                ;;
+        -v | --verify )         verifyBuild
+                                ;;
+        -u | --upload )         buildAndUpload
+                                ;;
+        -b | --backupConfig )   shift
+                                backupMarlinConfiguration $1
+                                ;;
+        -r | --restoreConfig )  shift
+                                installMarlinConfiguration $1
+                                ;;
+        -c | --clean )          shift
+                                cleanEverything 
+                                ;;
+        -h | --help )           printDocu
+                                ;;
+        * )                     printDocu
+                                exit 1
+    esac
+    shift
+done
